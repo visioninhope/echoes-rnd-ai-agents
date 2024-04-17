935802d526279d65fc339992c605ec6c4c3f3351
Rohittomar01 <rohittomar9285@gmail.com>
2024-04-17 12:50:05 +0530

    add more url to allow

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,14 +1,17 @@
 import { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
-  const sitemap = "https://acme.com/sitemap.xml"
+  const sitemap = "https://acme.com/sitemap.xml";
 
   console.log("sitemap", sitemap);
   return {
     rules: {
       userAgent: "*",
-      allow:"*", // Allow crawling by any bot
+      allow: [
+        "/dashboard/[slug]/chat/[chatid]",
+        "/dashboard/testtest/chat/3918",
+      ],
     },
-    sitemap: 'https://acme.com/sitemap.xml',
+    sitemap: "https://acme.com/sitemap.xml",
   };
 }
