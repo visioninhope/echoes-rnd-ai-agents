c09c76cbb2e8a576a7f922a196880a2ca31f9189
Rohittomar01 <rohittomar9285@gmail.com>
2024-04-17 12:28:02 +0530

    chnaged in robost.ts  allow:"*", // Allow crawling by any bot

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,7 +7,7 @@ export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: "*",
-      allow: "/dashboard/[slug]/chat/[chatid]",
+      allow:"*", // Allow crawling by any bot
     },
     sitemap: 'https://acme.com/sitemap.xml',
   };
