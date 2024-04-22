83d5dacff7ffbf247893921e24ec35e36ee8a80f
Rohittomar01 <rohittomar9285@gmail.com>
2024-04-23 01:03:02 +0530

    lint fixed

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -3,10 +3,10 @@ import { MetadataRoute } from "next";
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: '', // No path is disallowed
+      userAgent: "*",
+      allow: "/",
+      disallow: "", // No path is disallowed
     },
-    sitemap: 'https://acme.com/sitemap.xml',
-  }
+    sitemap: "https://acme.com/sitemap.xml",
+  };
 }
